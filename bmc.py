3e0b85c26a7e270e053b5f465dd62f2a79dbb0f6
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2021-12-05 02:36:27 +0700

    Update bmc.py

--- bmc.py
+++ bmc.py
@@ -29,6 +29,7 @@ def kontol():
 \x1b[1;97m [\x1b[1;94m\x1b[1;92m•\x1b[1;97m] Author   : Bangsat Militan ☆
 \x1b[1;97m [\x1b[1;92m•\x1b[1;94m\x1b[1;97m] Github   : Github.com/Bajingan-Z
 \x1b[1;97m [\x1b[1;94m\x1b[1;92m•\x1b[1;97m] Facebook : Angga
+\x1b[1;97m [\x1b[1;94m\x1b[1;92m•\x1b[1;97m] Youtube  : Angga-Z ™
 \x1b[1;94m────────────────────────────────────────────────────""")
 
 def jembut():
@@ -39,9 +40,10 @@ def jembut():
       ██║░░██║██║░╚███║╚██████╔╝╚██████╔╝██║░░██║
 \x1b[1;91m  •\x1b[1;93m•\x1b[1;92m•\x1b[1;97m ╚═╝░░╚═╝╚═╝░░╚══╝░╚═════╝░░╚═════╝░╚═╝░░╚═╝\x1b[1;92m •\x1b[1;93m•\x1b[1;91m•
 \x1b[1;94m────────────────────────────────────────────────────
-\x1b[1;97m [\x1b[1;92m•\x1b[1;97m] Author   : Bangsat Militan ☆
-\x1b[1;97m [\x1b[1;92m•\x1b[1;97m] Github   : Github.com/Bajingan-Z
-\x1b[1;97m [\x1b[1;92m•\x1b[1;97m] Facebook : Angga
+\x1b[1;97m [\x1b[1;94m\x1b[1;92m•\x1b[1;97m] Author   : Bangsat Militan ☆
+\x1b[1;97m [\x1b[1;94m\x1b[1;92m•\x1b[1;97m] Github   : Github.com/Bajingan-Z
+\x1b[1;97m [\x1b[1;94m\x1b[1;92m•\x1b[1;97m] Facebook : Angga
+\x1b[1;97m [\x1b[1;94m\x1b[1;92m•\x1b[1;97m] Youtube  : Angga-Z ™
 \x1b[1;94m────────────────────────────────────────────────────""")
 
 def yayanxd():
